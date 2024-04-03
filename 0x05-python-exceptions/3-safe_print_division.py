941db5afb49b24c35faa13436152b6665a6f0ece
senay <117833363+wizsenay@users.noreply.github.com>
2024-04-03 08:22:54 +0300

    Update 3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,5 +5,5 @@ def safe_print_division(a, b):
     except (ValueError, TypeError, ZeroDivisionError):
         div = None
     finally:
-        print("Inside result:{}".format(div))
+        print("Inside result: {}".format(div))
         return div
