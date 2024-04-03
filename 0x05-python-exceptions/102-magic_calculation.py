06ddb2237821e673e618dbf7366cd99f9b74def1
wizsenay <tomasfrehiwot@gmail.com>
2024-04-02 22:57:05 -0700

    add file

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    result = 0
+    try:
+        for i in range(1, 3):
+            if i > a:
+                raise Exception('Too far')
+            result += a ** b / i
+    except:
+        result = b + a
+    return result
