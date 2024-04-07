8c70cb4d7c0470e0a56284367ea131291dda9bed
wizsenay <tomasfrehiwot@gmail.com>
2024-04-07 12:31:50 -0700

    add a file 4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+""" A modules that contains a class Square that defines a square by:
+            (based on 0-square.py)
+
+     - Private instance attribute: size
+     - Instantiation with size (no type/value verification) """
+
+
+class Square:
+    """ defines a square by:
+                    Private instance attribute:
+                    size Instantiation with size """
+
+    def __init__(self, size=0):
+        """Instantiation with size (int)"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """return the value of privet attribut"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """add a new vale to the privet attribut '__size' """
+        if (not isinstance(value, int)):
+            raise TypeError("size must be an integer")
+        if (value < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """ublic instance method: def area(self):
+                 that returns the current square area"""
+        return (int(self.__size) ** 2)
