553f2085f4c19020802c51498e98184992024373
wizsenay <tomasfrehiwot@gmail.com>
2024-04-10 12:14:05 -0700

    adding some files

--- 0x08-python-more_classes/1-main.py
+++ 0x08-python-more_classes/1-main.py
@@ -7,3 +7,8 @@ print(my_rectangle.__dict__)
 my_rectangle.width = 10
 my_rectangle.height = 3
 print(my_rectangle.__dict__)
+
+try:
+    myrectangle = Rectangle(2, -3)
+except Exception as e:
+    print("[{}] {}".format(e.Rectangle.name_, e))
