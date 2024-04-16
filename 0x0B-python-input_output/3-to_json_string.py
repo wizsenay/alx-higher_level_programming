eb6e2a73a81d91df85c67794cb9b6fb60bdb4af5
wizsenay <tomasfrehiwot@gmail.com>
2024-04-16 11:41:35 -0700

    a file that returns the JSON representation of an object (string)

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import json
+
+""" a modules used to convert any data type to sting foramt(sting type) """
+
+
+def to_json_string(my_obj):
+    """
+       prototype
+       ---------
+       my_obj: Any
+         any data type input that want to covert str type
+
+       return
+       ------
+         str
+         the converted text
+    """
+    return json.dumps(my_obj)
