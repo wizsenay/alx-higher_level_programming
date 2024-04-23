3704433c8678b3e37ae00dfbf88cbbb5dcc017e6
wizsenay <tomasfrehiwot@gmail.com>
2024-04-23 09:19:17 -0700

    add file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,16 +1,30 @@
 #!/usr/bin/python3
 
-"""
-   A base class used to give an id and maneageid attribute
-        in all your future classes and to avoid duplicating the same code
-"""
+"""This modlus provide a class used to give an id for the instancess"""
 
 
-class Base:
+class base:
+    """
+    A base class used to give id for every instancess
+    ...
+    Attributes
+    ----------
+    __nb_objects : int
+          assign the new value to the public instance attribute id
+    id : int
+       The id of the given instance
+    """
+
     __nb_objects = 0
+
     def __init__(self, id=None):
-        #__nb_objects = 0
-        if id != None:
+        """
+        parameters
+        ----------
+        id : int
+           The id of the given instance
+        """
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
