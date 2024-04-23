f9978670fe8789ec071a259bfc37db7be415b564
senay <117833363+wizsenay@users.noreply.github.com>
2024-04-23 20:47:14 +0300

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -50,40 +50,48 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """width getter"""
+        """width getter
+        """
         return self.__width
 
     @property
     def height(self):
-        """heigth getter"""
+        """height getter
+        """
         return self.__height
 
     @property
     def x(self):
-        """x getter"""
+        """x getter
+        """
         return self.__x
 
     @property
     def y(self):
-        """y getter"""
+        """y getter
+        """
         return self.__y
 
     @width.setter
     def width(self, new_width):
-        """width setter"""
+        """width setter
+        """
         self.__width = new_width
 
     @height.setter
     def height(self, new_height):
-        """height setter"""
+        """height setter
+        """
         self.__height = new_height
 
     @x.setter
     def x(self, new_x):
-        """x setter"""
+        """x setter
+        """
         self.__x = new_x
 
     @y.setter
     def y(self, new_y):
-        """y setter"""
+        """y setter
+        """
         self.__y = new_y
