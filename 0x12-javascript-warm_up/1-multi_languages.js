e674f86618d3cc2bc52378278644521e7fd413e1
wizsenay <tomasfrehiwot@gmail.com>
2024-06-18 11:49:53 -0700

    update

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,6 +2,8 @@
 
 /*  a script that prints 3 lines*/
 
-console.log('C is fun')
-console.log('Python is cool')
-console.log('JavaScript is amazing')
+const str_print = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+
+for (let i = 0; i <= 2; i++) {
+  console.log(str_print[i]
+}
