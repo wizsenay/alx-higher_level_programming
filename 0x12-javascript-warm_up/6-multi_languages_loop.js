75084eb7a90736cf01d4c50248adab8adbd7caa3
wizsenay <tomasfrehiwot@gmail.com>
2024-06-19 00:11:18 +0300

    add file  6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+/* a script that prints 3 lines */
+
+const strPrint = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i <= 2; i++) {
+  console.log(strPrint[i]);
+}
