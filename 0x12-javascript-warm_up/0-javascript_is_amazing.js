516fc4950ce21907c922c11c05fc98d6f1fe3a74
wizsenay <tomasfrehiwot@gmail.com>
2024-06-18 11:32:39 -0700

    add file

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
 
+/* a script that prints “JavaScript is amazing” */
+
 let myVar
 myVar = 'JavaScript is amazing'
 
---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+/*  a script that prints 3 lines*/
+
+const str_print = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+
+for (let i = 0; i <= 2; i++) {
+  console.log(str_print[i])
+}
