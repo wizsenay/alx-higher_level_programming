19b5a6d30914b0e752ec24a020a465f334cedeb7
wizsenay <tomasfrehiwot@gmail.com>
2024-06-19 22:49:45 +0300

    update

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,24 +1,18 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+const copSquare = require('./5-square');
 
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-    this.size = size;
-  }
-
+class Square extends copSquare {
   charPrint (c) {
     if (c === undefined) {
-      super.print();
-    } else {
-      for (let i = 0; i < this.size; i++) {
-        let line = '';
-        for (let j = 0; j < this.size; j++) {
-          line += c;
-        }
-        console.log(line);
+      c = 'X';
+    }
+    for (let i = 0; i < this.width; i++) {
+      let line = '';
+      for (let j = 0; j < this.width; j++) {
+         line += c;
       }
+      console.log(line);
     }
   }
 }
