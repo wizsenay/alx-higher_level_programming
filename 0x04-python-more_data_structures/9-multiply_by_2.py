f7733d242fdceaffecccb0f0f39334690671e564
wizsenay <tomasfrehiwot@gmail.com>
2025-02-21 06:50:03 +0300

    update - make the code  to multiplay each values by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -5,9 +5,9 @@ def multiply_by_2(a_dictionary):
     # new_dictionary = {}
     # for key, val in a_dictionary.items():
     #    if val % 2 == 0:
-    #        new_dictionary[key] = val
+    #        new_dictionary[key] = val * 2
     new_dictionary = {
-        key: value
+        key: value * 2
         for key, value in a_dictionary.items()
         if value % 2 == 0
     }
