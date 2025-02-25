db822e86644d5d1022e61c37aba730f5510f9f90
wizsenay <tomasfrehiwot@gmail.com>
2025-02-25 23:45:06 +0300

    update

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,7 +4,7 @@
 def best_score(a_dictionary):
     num = 0
     name = ''
-    if a_dictionary is None or not dictionary:
+    if a_dictionary is None or not a_dictionary:
         return None
     for key, val in a_dictionary.items():
         if val > num:
